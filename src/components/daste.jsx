56dbbe6f6a895103f5bccb73347b60f17e46e88c
onyx <mohsenwaller@gmail.com>
2025-02-24 23:24:56 +0330

    daste ha added & border-b animation

---
+++ src/components/daste.jsx
@@ -0,0 +1,12 @@
+export default function Daste(){
+    return(
+        
+            <ul className=' w-35 space-y-5 absolute hidden group-hover:block pb-3 pt-3 bg-white -z-1'>
+                <li className='hover:bg-gray-100 text-right hover:text-red-500 cursor-pointer transition-colors duration-200'>کالای دیجیتال</li>
+                <li className='hover:bg-gray-100 text-right hover:text-red-500 cursor-pointer transition-colors duration-200'>زیبایی و سلامت</li>
+                <li className='hover:bg-gray-100 text-right hover:text-red-500 cursor-pointer transition-colors duration-200'>خانه و آشپزخانه</li>
+                <li className='hover:bg-gray-100 text-right hover:text-red-500 cursor-pointer transition-colors duration-200'>مد و پوشاک</li>
+            </ul>
+        
+    )
+}
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,8 @@
-
+import Daste from './daste';
 
 export default function Header1(){
     return(
       <header className='shadow-lg bg-white'>
-        
         <div className='fixed bg-white left-0 right-0 z-50 font-medium text-1xl  top-0 flex p-3 justify-between h-18 items-center  '>
       
       <div className='flex gap-5 w-2/12 items-center  '>
@@ -18,7 +17,7 @@ export default function Header1(){
       </div>
 
 
-        <div className='  flex h-8/12 items-center w-8/12 gap-4'>
+        <div className='  flex h-8/12 items-center w-7/12 gap-4'>
 
          <div className='h-15/12 bg-neutral-100 grow rounded-md px-0 flex'>
 
@@ -36,13 +35,15 @@ export default function Header1(){
       </div>
 
       <nav className='flex top-0 sticky gap-5 text-right items-center pr-3 w-full mt-18 p-2 shadow-lg' dir="rtl">
-        <span className="opacity-85 cursor-pointer border-b-2 hover:border-red-500 border-transparent">دسته بندی کالاها</span>
+        <span className='group opacity-85 cursor-pointer relative after:content-[""] after:absolute after:w-full after:h-0.5 hover:after:bg-red-600 after:right-0 after:top-[2rem] after:transition-all after:duration-300'>دسته بندی کالاها
+        <Daste/>
+        </span>
         <div className='opacity-50 inline-flex gap-2'>
-          <div href='#' className='cursor-pointer border-b-2 hover:border-red-500 border-transparent  '>شگفت انگیز</div>
-          <div href='#' className='cursor-pointer border-b-2 hover:border-red-500 border-transparent  '>سوپر مارکت</div>
-          <div href='#' className='cursor-pointer border-b-2 hover:border-red-500 border-transparent  '>طلای دیجیتال</div>
-          <div href='#' className='cursor-pointer border-b-2 hover:border-red-500 border-transparent  '>پرفروش ترینها</div>
-          <div href='#' className='cursor-pointer border-b-2 hover:border-red-500 border-transparent  '>خرید کالا کارکرده</div>
+          <div href='#' className='cursor-pointer relative after:content-[""] after:absolute after:w-full after:h-0.5 hover:after:bg-red-600 after:right-0 after:top-[2rem] after:transition-all after:duration-300'>شگفت انگیز</div>
+          <div href='#' className='cursor-pointer relative after:content-[""] after:absolute after:w-full after:h-0.5 hover:after:bg-red-600 after:right-0 after:top-[2rem] after:transition-all after:duration-300'>سوپر مارکت</div>
+          <div href='#' className='cursor-pointer relative after:content-[""] after:absolute after:w-full after:h-0.5 hover:after:bg-red-600 after:right-0 after:top-[2rem] after:transition-all after:duration-300'>طلای دیجیتال</div>
+          <div href='#' className='cursor-pointer relative after:content-[""] after:absolute after:w-full after:h-0.5 hover:after:bg-red-600 after:right-0 after:top-[2rem] after:transition-all after:duration-300'>پرفروش ترینها</div>
+          <div href='#' className='cursor-pointer relative after:content-[""] after:absolute after:w-full after:h-0.5 hover:after:bg-red-600 after:right-0 after:top-[2rem] after:transition-all after:duration-300'>خرید کالا کارکرده</div>
         </div>
         </nav>
       
