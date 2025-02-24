dc558abc19641cb5c4e42be986bb9544bd5cefe8
onyx <mohsenwaller@gmail.com>
2025-02-24 19:33:48 +0330

    header left

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,14 +4,19 @@ export default function Header1(){
     return(
         <header className='bg-cyan-950 font-medium text-1xl sticky top-0 flex p-3 justify-between h-18 items-center '>
       
-      <div className=''>
+      <div className='flex gap-5 w-2/12 items-center  '>
 
-      <a href='#' className='bg-green-600 rounded-sm transition delay-150 duration-300 ease-in-out m-1 p-1 '>login</a>
+      <img className='cursor-pointer '   src="./src/images/sabad.svg" alt="sabad-kharid" />
+
+      <a href='#' className='inline-flex p-3 gap-2 h-9 border-2 border-gray-400 items-center rounded-md'>
+        <button className='cursor-pointer opacity-90'>ورود | ثبت نام</button>
+        <img className='opacity-60 w-5' src="./src/images/login.svg" alt="arrow-down" />
+      </a>
 
       </div>
 
 
-        <div className='flex h-8/12 items-center w-200 gap-4'>
+        <div className='flex h-8/12 items-center w-8/12 gap-4'>
 
          <div className='h-15/12 bg-neutral-100 grow rounded-md px-0 flex'>
 
