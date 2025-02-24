b77a287b549f192dd1f937cdb3162314fec82dd8
onyx <mohsenwaller@gmail.com>
2025-02-24 13:09:51 +0330

    fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,17 +2,25 @@
 
 export default function Header1(){
     return(
-        <header className='bg-cyan-950 font-medium text-1xl sticky top-0 flex p-3 justify-between h-15 items-center '>
+        <header className='bg-cyan-950 font-medium text-1xl sticky top-0 flex p-3 justify-between h-18 items-center '>
       
       <div className=''>
+
       <a href='#' className='bg-green-600 rounded-sm transition delay-150 duration-300 ease-in-out m-1 p-1 '>login</a>
+
       </div>
 
-        <div className='flex items-center w-200 gap-4'>
 
-        <div className=' bg-neutral-100 grow rounded px-0 lg:px-4 text-body-2'>
-         <input className='w-full h-full text-right text-neutral-500' type="text" placeholder='جستجو ' />
-        </div>
+        <div className='flex h-8/12 items-center w-200 gap-4'>
+
+         <div className='h-15/12 bg-neutral-100 grow rounded-md px-0 flex'>
+
+         <input className='w-full h-full text-right text-neutral-500' type="text" placeholder='جستجو' />
+
+         <img className='flex cursor-pointer opacity-25 size-7 m-2' src="./src/images/search.svg" alt="search" />
+
+         </div>
+
         <img  className='w-50 inline-block' src="./src/images/logo.svg" alt="digikala" />
        
         </div>
